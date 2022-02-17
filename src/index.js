b12beca20c36455fb3d9af476bbd2a297b3ec897
Nicholas Berlette <nick@berlette.com>
2022-02-16 22:37:18 -0800

    Update configurations, etc.

--- src/index.js
+++ src/index.js
@@ -1,75 +1,94 @@
-/**
- * @module thizz/dist/index.js
- * @package thizz
- * @version 1.0.5
- * @license MIT
- * @author Nicholas Berlette <nick@berlette.com> (https://github.com/nberlette)
- * @preserve MIT (c) 2022 Nicholas Berlette <nick@berlette.com>
- */
-export * from './isArray';
-export * from './isArrayBuffer';
-export * from './isArrayLike';
-export * from './isBigInt';
-export * from './isBoolean';
-export * from './isBuffer';
-export * from './isCatchable';
-export * from './isDate';
-export * from './isError';
-export * from './isFunction';
-export * from './isInteger';
-export * from './isMap';
-export * from './isNaN';
-export * from './isNil';
-export * from './isNull';
-export * from './isNumber';
-export * from './isObject';
-export * from './isObjectLiteral';
-export * from './isPromise';
-export * from './isProxy';
-export * from './isRegExp';
-export * from './isSafeInteger';
-export * from './isSet';
-export * from './isString';
-export * from './isStringObject';
-export * from './isSymbol';
-export * from './isThenable';
-export * from './isUndefined';
-export * from './isWeakMap';
-export * from './isWeakSet';
+import {isArray} from 'thizz/isArray';
+import {isArrayBuffer} from 'thizz/isArrayBuffer';
+import {isArrayLike} from 'thizz/isArrayLike';
+import {isBigInt} from 'thizz/isBigInt';
+import {isBoolean} from 'thizz/isBoolean';
+import {isBuffer} from 'thizz/isBuffer';
+import {isCatchable} from 'thizz/isCatchable';
+import {isDate} from 'thizz/isDate';
+import {isError} from 'thizz/isError';
+import {isFunction} from 'thizz/isFunction';
+import {isInteger} from 'thizz/isInteger';
+import {isMap} from 'thizz/isMap';
+import {isNaN} from 'thizz/isNaN';
+import {isNil} from 'thizz/isNil';
+import {isNull} from 'thizz/isNull';
+import {isNumber} from 'thizz/isNumber';
+import {isObject} from 'thizz/isObject';
+import {isObjectLiteral} from 'thizz/isObjectLiteral';
+import {isPromise} from 'thizz/isPromise';
+import {isProxy} from 'thizz/isProxy';
+import {isRegExp} from 'thizz/isRegExp';
+import {isSafeInteger} from 'thizz/isSafeInteger';
+import {isSet} from 'thizz/isSet';
+import {isString} from 'thizz/isString';
+import {isStringObject} from 'thizz/isStringObject';
+import {isSymbol} from 'thizz/isSymbol';
+import {isThenable} from 'thizz/isThenable';
+import {isUndefined} from 'thizz/isUndefined';
+import {isWeakMap} from 'thizz/isWeakMap';
+import {isWeakSet} from 'thizz/isWeakSet';
 
-// export {
-// 	isArray,
-// 	isArrayBuffer,
-// 	isArrayLike,
-// 	isBigInt,
-// 	isBoolean,
-// 	isBuffer,
-// 	isCatchable,
-// 	isDate,
-// 	isError,
-// 	isFunction,
-// 	isInteger,
-// 	isMap,
-// 	isNaN,
-// 	isNil,
-// 	isNull,
-// 	isNumber,
-// 	isObject,
-// 	isObjectLiteral,
-// 	isPromise,
-// 	isProxy,
-// 	isRegExp,
-// 	isSafeInteger,
-// 	isSet,
-// 	isString,
-// 	isStringObject,
-// 	isSymbol,
-// 	isThenable,
-// 	isUint16Array,
-// 	isUint32Array,
-// 	isUint8Array,
-// 	isUint8ClampedArray,
-// 	isUndefined,
-// 	isWeakMap,
-// 	isWeakSet,
-// };
+export * from 'thizz/isArray';
+export * from 'thizz/isArrayBuffer';
+export * from 'thizz/isArrayLike';
+export * from 'thizz/isBigInt';
+export * from 'thizz/isBoolean';
+export * from 'thizz/isBuffer';
+export * from 'thizz/isCatchable';
+export * from 'thizz/isDate';
+export * from 'thizz/isError';
+export * from 'thizz/isFunction';
+export * from 'thizz/isInteger';
+export * from 'thizz/isMap';
+export * from 'thizz/isNaN';
+export * from 'thizz/isNil';
+export * from 'thizz/isNull';
+export * from 'thizz/isNumber';
+export * from 'thizz/isObject';
+export * from 'thizz/isObjectLiteral';
+export * from 'thizz/isPromise';
+export * from 'thizz/isProxy';
+export * from 'thizz/isRegExp';
+export * from 'thizz/isSafeInteger';
+export * from 'thizz/isSet';
+export * from 'thizz/isString';
+export * from 'thizz/isStringObject';
+export * from 'thizz/isSymbol';
+export * from 'thizz/isThenable';
+export * from 'thizz/isUndefined';
+export * from 'thizz/isWeakMap';
+export * from 'thizz/isWeakSet';
+
+export default{
+	isArray,
+	isArrayBuffer,
+	isArrayLike,
+	isBigInt,
+	isBoolean,
+	isBuffer,
+	isCatchable,
+	isDate,
+	isError,
+	isFunction,
+	isInteger,
+	isMap,
+	isNaN,
+	isNil,
+	isNull,
+	isNumber,
+	isObject,
+	isObjectLiteral,
+	isPromise,
+	isProxy,
+	isRegExp,
+	isSafeInteger,
+	isSet,
+	isString,
+	isStringObject,
+	isSymbol,
+	isThenable,
+	isUndefined,
+	isWeakMap,
+	isWeakSet,
+};
